713932c8c8d5069c249acd6638e42303d6cd6414
Kai Kunze <kaikunze@icloud.com>
2013-11-24 19:12:32 +0900

    Initial Commit

---
+++ Gait/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gait
+//
+//  Created by Kai Kunze on 24/11/2013.
+//  Copyright (c) 2013 Kai Kunze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GaitTests/GaitTests.m
@@ -0,0 +1,34 @@
+//
+//  GaitTests.m
+//  GaitTests
+//
+//  Created by Kai Kunze on 24/11/2013.
+//  Copyright (c) 2013 Kai Kunze. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GaitTests : XCTestCase
+
+@end
+
+@implementation GaitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
